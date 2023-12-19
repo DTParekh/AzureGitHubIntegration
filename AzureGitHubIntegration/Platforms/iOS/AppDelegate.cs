6d9f3c2d68169a22d29efc8123f4c85888f4783b
Dharmesh Parekh <dharmesh_parekh@hotmail.com>
2023-12-19 07:47:49 -0600

    Add project files.

---
+++ AzureGitHubIntegration/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AzureGitHubIntegration
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
